02bf4233509eb82405d6517ec41057cb3db3f274
Marc Klesiewicz <marcklesiewicz21@gmail.com>
2022-02-13 15:20:23 +0100

    Added recipe detail and gesturedetector

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'recipe.dart';
+import 'recipe_detail.dart';
 
 void main() {
   runApp(const RecipeApp());
@@ -13,6 +14,7 @@ class RecipeApp extends StatelessWidget {
   Widget build(BuildContext context) {
     final ThemeData theme = ThemeData();
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Recipe Caluculator',
       theme: theme.copyWith(
         colorScheme: theme.colorScheme.copyWith(
@@ -20,7 +22,6 @@ class RecipeApp extends StatelessWidget {
           secondary: Colors.black,
         ),
       ),
-
       home: const MyHomePage(title: 'Recipe Calculator'),
     );
   }
@@ -28,15 +29,6 @@ class RecipeApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -46,7 +38,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
@@ -54,20 +46,30 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: SafeArea(
         child: ListView.builder(
-          itemCount: Recipe.samples.length,
-          itemBuilder: (BuildContext context, int index){
-            return buildRecipeCard(Recipe.samples[index]);
-          }
-        ),
+            itemCount: Recipe.samples.length,
+            itemBuilder: (BuildContext context, int index) {
+              return GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return RecipeDetail(recipe: Recipe.samples[index]);
+                      },
+                    ),
+                  );
+                },
+                child: buildRecipeCard(Recipe.samples[index]),
+              );
+            }),
       ),
     );
   }
 
-  Widget buildRecipeCard(Recipe recipe){
+  Widget buildRecipeCard(Recipe recipe) {
     return Card(
       elevation: 2.0,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10.0)),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
       child: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
@@ -87,7 +89,6 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-        
     );
   }
 }
